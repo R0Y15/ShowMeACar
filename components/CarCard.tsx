1688d0e6b4ad33044d9190f951657f24bec1ad1d
R0Y15 <bishalroy15jan@gmail.com>
2023-12-28 00:29:13 +0530

    minor changes

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -14,7 +14,7 @@ interface CarCardProps {
 
 const CarCard = ({ car }: CarCardProps) => {
   const { city_mpg, combination_mpg, cylinders, displacement, drive, fuel_type, highway_mpg, make, model, transmission, year } = car;
-const [showCarDetails, setShowCarDetails] = useState(false);
+  const [showCarDetails, setShowCarDetails] = useState(false);
   const carRent = calculateCarRent(city_mpg, year);
 
   return (
@@ -35,7 +35,7 @@ const [showCarDetails, setShowCarDetails] = useState(false);
       </p>
 
       <div className="relative w-full h-40 my-3 object-contain">
-        <Image src={generateImage(car)} alt='car-model' fill priority className='object-contain'/>
+        <Image src={generateImage(car)} alt='car-model' fill priority className='object-contain' />
       </div>
 
       <div className="relative flex w-full mt-2">
@@ -46,29 +46,29 @@ const [showCarDetails, setShowCarDetails] = useState(false);
               {transmission === 'a' ? 'Automatic' : 'Manual'}
             </p>
           </div>
-          
+
           <div className="flex flex-col justify-center items-center gap-2">
             <Image src={"/tire.svg"} alt="tire" width={20} height={20} />
             <p className="text-[14px]">
               {drive.toLocaleUpperCase()}
             </p>
           </div>
-          
+
           <div className="flex flex-col justify-center items-center gap-2">
             <Image src={"/gas.svg"} alt="gas" width={20} height={20} />
             <p className="text-[14px]">
               {city_mpg} kmpl
             </p>
           </div>
-          
+
         </div>
         <div className="car-card__btn-container">
-          <CustomButton 
-          title='More Details'
-          containerStyles='w-full py-[16px] rounded-full bg-primary-blue'
-          textStyles='text-white text-[14px] leading-[17px] font-bold'
-          rightIcon='right-arrow.svg' 
-          handleClick={() => setShowCarDetails(true)}
+          <CustomButton
+            title='More Details'
+            containerStyles='w-full py-[16px] rounded-full bg-primary-blue'
+            textStyles='text-white text-[14px] leading-[17px] font-bold'
+            rightIcon='right-arrow.svg'
+            handleClick={() => setShowCarDetails(true)}
           />
         </div>
       </div>
